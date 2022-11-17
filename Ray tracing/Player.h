5fcdc8149179c5077e6e3a569830f5bfc572b396
Imantion <daniil.kolomiets.00@gmail.com>
2022-11-17 18:26:11 +0200

    buttons scroll

--- Ray tracing/Player.h
+++ Ray tracing/Player.h
@@ -7,324 +7,144 @@ using namespace sf;
 
 double rotation = 180.0 / PI;
 
-float distance(float x1, float y1, float x2, float y2)
-{
-	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
-
 class Player
 {
 private:
 
-	Vector2i mousePos;
 	Vector2f pos;
-	Vector2f acceleration;
+	Vector2f speed;
 	RectangleShape player;
 	RectangleShape direction;
-	std::vector<VertexArray> ray;
+	VertexArray ray;
 	float rotate;
 
 public:
-	
+
 	Player(Vector2f pos)
 	{
-		
 		rotate = 0;
 		this->pos = pos;
 
 		player.setSize(Vector2f(10, 10));
-		player.setOrigin(Vector2f(5, 5));
+		player.setOrigin(Vector2f(10, 5));
 		player.setFillColor(Color(162, 25, 255));
 
 		direction.setSize(Vector2f(15, 3));
 		direction.setOrigin(Vector2f(0, 1.5));
 		direction.setFillColor(Color(162, 25, 255));
 
-		ray.resize(60);
-		for (size_t i = 0; i < ray.size(); i++)
-		{
-			ray[i].resize(2);
-			ray[i].setPrimitiveType(LineStrip);
-		}
+		ray.resize(8);
+		ray.setPrimitiveType(LineStrip);
 	}
 
 	void setSpeed(Vector2f speed)
 	{
-		this->acceleration = speed;
+		this->speed = speed;
 	}
 
 	void setPos(Vector2f pos)
 	{
 		this->pos = pos;
 	}
-	void rotatePlayer(int ang)
-	{
-		rotate += ang;
-	}
-	void update(const float &time, RenderWindow& window)
-	{
-		/*rotate = angleMouse(window);*/
-		
 
-		control(time);
-
+	void update()
+	{
+		control();
 		player.setPosition(pos);
-
 		direction.setPosition(pos);
-
-		for (size_t i = 0; i < ray.size(); i++)
-		{
-			ray[i][0].position = pos;
-		}
-		
 	}
 
-	void drawRays3D(RenderWindow& window)
+	void drawRays3D()
 	{
 		int r, mx, my, mp, dof; float rx, ry, ra, xo, yo;
-		float dist;
-		RectangleShape wall;
-
-		RectangleShape floor;
-		floor.setFillColor(Color(164, 12, 240));
-
-		RectangleShape roof;
-		roof.setFillColor(/*Color(12, 240, 233)*/Color::Blue);
-		
-
-		for (r = 0; r < 60; r++)
+		ra = abs(rotate) / rotation;
+		for (r = 0; r < 1; r++)
 		{
-			float distH = 10000;
-			ra = rotate - 30 + r;
-			if (ra < 0) ra += 360;
-			else if (ra > 360) ra -= 360;
-			// vertical
 			dof = 0;
-			float aTan = 1 / tan(ra / rotation);
-			if (ra < 180)
+			float aTan = -1 / tan(ra);
+			if (ra > PI)
 			{
-				ry = int(pos.y / 64) * 64 - 0.0001;
+				ry = (((int)pos.y >> 6) << 6) - 0.0001;
 				rx = (pos.y - ry) * aTan + pos.x;
 				yo = -64; xo = -yo * aTan;
 			}
-			if (ra > 180)
+			if (ra < PI)
 			{
-				ry = int(pos.y / 64) * 64 + 64;
+				ry = (((int)pos.y >> 6) << 6) + 64;
 				rx = (pos.y - ry) * aTan + pos.x;
 				yo = 64; xo = -yo * aTan;
 			}
-			if (ra == 0 || ra == 360 || ra == 180) { rx = pos.x; ry = pos.y; dof = 8; }
 			while (dof < 8)
 			{
 				mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * mapX + mx;
 
+				ray[dof].position = Vector2f(rx, ry);
+				ray[dof].color = Color(0, 0, 255);
 
-				if (mp > 0 && mp < mapX * mapY && map[mp] == 1)
+				if (mp < mapX * mapY && map[mp] == 1)
 				{
-					distH = distance(pos.x, pos.y, rx, ry);
+					for (size_t i = dof + 1; i < 8; i++)
+					{
+						ray[i].position = Vector2f(rx, ry);
+						ray[i].color = Color(0, 0, 255);
+					}
 
-					ray[r][1].position = Vector2f(rx, ry);
-					ray[r][1].color = Color::Blue;
-					
 					dof = 8;
 				}
 				else
 				{
 					rx += xo; ry += yo;
-					++dof;
 				}
 
 			}
-
-			// horizontal
-
-
-			float nTan = tan(ra / rotation);
-			float distW = 10000;
-			dof = 0;
-			if (ra > 90 && ra < 270)
-			{
-				rx = int(pos.x / 64) * 64 - 0.0001;
-				ry = (pos.x - rx) * nTan + pos.y;
-				xo = -64; yo = -xo * nTan;
-			}
-
-			if (ra < 90 || ra > 270)
-			{
-				rx = int(pos.x / 64) * 64 + 64;
-				ry = (pos.x - rx) * nTan + pos.y;
-				xo = 64; yo = -xo * nTan;
-			}
-
-			if (ra == 90 || ra == 270) { rx = pos.x; ry = pos.y; dof = 8; }
-			while (dof < 8)
-			{
-				mx = (int)(rx) >> 6; my = (int)(ry) >> 6; mp = my * mapX + mx;
-
-
-				if (mp > 0 && mp < mapX * mapY && map[mp] == 1)
-				{
-					distW = distance(pos.x, pos.y, rx, ry);
-					dof = 8;
-				}
-				else
-				{
-					rx += xo; ry += yo;
-					++dof;
-				}
-			}
-			if (distH > distW)
-			{
-				ray[r][1].position = Vector2f(rx, ry);
-				ray[r][1].color = Color::Blue;
-				dist = distW;
-
-				wall.setFillColor(Color(255,0,0));
-			}
-			else { dist = distH; wall.setFillColor(Color(240, 0, 0)); }
-			window.draw(ray[r]);
-
-			
-			// Draw 3D
-			float ca = rotate - ra; if (ca < 0) ca += 360; if (ca > 360) ca -= 360;
-			dist = dist * cos(ca / rotation);
-
-			float lineH = (mapS * 512) / dist; if (lineH > 512) lineH = 512;	
-			float lineO = 256 - lineH / 2;
-
-			wall.setSize(Vector2f(10, lineH));
-			wall.setPosition(Vector2f(10 * r + 512, lineO));
-			window.draw(wall);
-
-			floor.setSize(Vector2f(10, 512 - lineH - lineO));
-			floor.setPosition(Vector2f(10 * r + 512, lineH + lineO));
-			window.draw(floor);
-
-			roof.setSize(Vector2f(10, lineO));
-			roof.setPosition(Vector2f(10 * r   + 512, 0));
-			window.draw(roof);
-			
-
 		}
 
 	}
 
 	void draw(RenderWindow& window)
 	{
-		player.setRotation(-rotate);
-		direction.setRotation(-rotate);
-		
+		rotate = angelMouse(window);
+
+
+		player.setRotation(rotate);
+		direction.setRotation(rotate);
+
 		window.draw(player);
 		window.draw(direction);
+		window.draw(ray);
+
 	}
 
-	float angleMouse(RenderWindow& window)
+	float angelMouse(RenderWindow& window)
 	{
 		Vector2f mousePos = window.mapPixelToCoords(Mouse::getPosition(window));
 
 		float diffX = mousePos.x - pos.x;
 		float diffY = mousePos.y - pos.y;
 
-		/*rotate = diffY / diffX;
+		float rot = atan2(diffX, diffY) * rotation;
 
-		if (rotate < 0) rotate += 2 * PI;
-		else if (rotate > 2 * PI) rotate -= 2 * PI;*/
-
-		float rot = atan2(diffY,diffX);
-
-		if (rot < 0) rot *= -1;
-		else rot = 2 * PI - rot;
-
-		std::cout << rot * rotation << std::endl;
-		return rot * rotation;
+		std::cout << rot << std::endl;
+		return rot;
 	}
 
-	void control(const float& time)
+	void control()
 	{
-		float rad = rotate / rotation;
-
-		Vector2i mousePos = Mouse::getPosition();
-
-	/*	float pdx = 1.5 * cos(rad);
-		float pdy = 1.5 * sin(rad);*/
-
-		float cs = time * 0.1 * cos(rad);
-		float sn = time * 0.1 * sin(rad);
-
-		float xo = 0, yo = 0;
-
-		if (cs > 0) xo = 20; else xo = -20;
-		if (sn < 0) yo = 20; else yo = -20;
-
-
-
-		
-
-		int ipx = pos.x / 64, ipx_sub = (pos.x - xo) / 64, ipx_add = (pos.x + xo) / 64;
-		int ipy = pos.y / 64, ipy_sub = (pos.y - yo) / 64, ipy_add = (pos.y + yo) / 64;
-
-
 		if (Keyboard::isKeyPressed(Keyboard::W))
 		{
-			if (map[ipy * mapX + ipx_add] == 0) this->pos.x += cs;
-			
-			if (map[ipy_add*mapX + ipx] == 0) this->pos.y -= sn;
-
+			this->pos.y -= 2;
 		}
 		if (Keyboard::isKeyPressed(Keyboard::A))
 		{
-			if (cs > 0) xo = 20; else xo = -20;
-			if (sn > 0) yo = 20; else yo = -20;
-
-			ipx = pos.x / 64, ipx_sub = (pos.x - xo) / 64, ipx_add = (pos.x + xo) / 64;
-			ipy = pos.y / 64, ipy_sub = (pos.y - yo) / 64, ipy_add = (pos.y + yo) / 64;
-
- 			if (map[ipy * mapX + ipx_add] == 0) this->pos.x += sn;
-			if (map[ipy_add * mapX + ipx] == 0)this->pos.y += cs;
-		
+			this->pos.x -= 2;
 		}
 		if (Keyboard::isKeyPressed(Keyboard::S))
 		{
-			
-			if (map[ipy * mapX + ipx_sub] == 0) this->pos.x -= cs;
-			if (map[ipy_sub * mapX + ipx] == 0)this->pos.y += sn;
-
-		
-			
+			this->pos.y += 2;
 		}
 		if (Keyboard::isKeyPressed(Keyboard::D))
 		{
-			if (cs > 0) xo = 20; else xo = -20;
-			if (sn > 0) yo = 20; else yo = -20;
-
-			ipx = pos.x / 64, ipx_sub = (pos.x - xo) / 64, ipx_add = (pos.x + xo) / 64;
-			ipy = pos.y / 64, ipy_sub = (pos.y - yo) / 64, ipy_add = (pos.y + yo) / 64;
-
-			if (map[ipy * mapX + ipx_add] == 0) this->pos.x -= sn;
-			if (map[ipy_add * mapX + ipx] == 0) this->pos.y -= cs;
-			
-		}
-
-		Vector2i mouseSecondPosition = Mouse::getPosition();
-		
-		
-	
-		if (mouseSecondPosition.x <  mousePos.x) rotate -=  15;
-		else if (mouseSecondPosition.x > mousePos.x) rotate +=  15;
-
-		if (rotate < 0) rotate += 360;
-		else if (rotate > 360) rotate -= 360; 
-	}
-
-
-	void collision()
-	{
-		for (size_t y = 0; y < 8; y++)
-		{
-			for (size_t x = 0; x < 8; x++)
-			{
-				/*if(player.getTextureRect().contains)*/
-			}
+			this->pos.x += 2;
 		}
 	}
 };
--- Ray tracing/main.cpp
+++ Ray tracing/main.cpp
@@ -5,44 +5,32 @@ using namespace sf;
 int main()
 {
 
-	
+
 	RenderWindow window(VideoMode(1024, 512), "Ray Tracing");
 	window.setFramerateLimit(60);
 
-	Player a(Vector2f(100,200));
-	float time;
-	Clock cl;
+
+	Player a(Vector2f(100, 200));
 	while (window.isOpen())
 	{
-		
-		Mouse::setPosition(Vector2i(512, 512));
-		time = cl.getElapsedTime().asMicroseconds();
-		cl.restart();
-		time = time / 800;
-		
-		
-		
+
+		a.update();
+
 
 		std::cout << "X: " << Mouse::getPosition(window).x << " Y: " << Mouse::getPosition(window).y << std::endl;
 		Event event;
 		while (window.pollEvent(event))
 		{
-			
-			if (Event::MouseMoved)
-			{
-				if (Mouse::getPosition().x > 512) a.rotatePlayer(time*0.1);
-				if (Mouse::getPosition().x < 512) a.rotatePlayer(-0.1*time);
-			}
 
 			if (event.type == Event::Closed)
+
 				window.close();
 		}
 
 		window.clear();
-		a.update(time, window);
 		drawMap(window);
 		a.draw(window);
-		a.drawRays3D(window);
+		a.drawRays3D();
 		window.display();
 	}
 
